0b92883a45d34a100a6afa2292ad2cbf50570062
Ivan Ryzhkov <ivan.ryzhkow@gmail.com>
2026-01-09 15:54:43 +0100

    Add GKE cluster module configuration

--- main.tf
+++ main.tf
@@ -75,3 +75,10 @@ data "google_container_cluster" "main" {
   # Location (region)
   location = var.GOOGLE_REGION
 }
+
+module "gke_cluster" {
+  source         = "github.com/<ВАШ-РЕПОЗИТОРІЙ>/tf-google-gke-cluster"
+  GOOGLE_REGION  = var.GOOGLE_REGION
+  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  GKE_NUM_NODES  = 2
+}
